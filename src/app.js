fb6d237d29987c6256bc43825a8597f173279d75
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2026-04-13 04:39:53 -0400

    Mount leaderboard routes and rename user router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express"
-import router from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js"
+import leaderboardRouter from "./routes/leaderboard.routes.js"
+
 import { rateLimiter } from "./middleware/rateLimiter.js"
 
 const app = express()
@@ -7,7 +9,8 @@ const app = express()
 app.use(rateLimiter(5, 60))
 
 app.use(express.json())
-app.use("/", router)
+app.use("/", userRouter)
+app.use("/leaderboard", leaderboardRouter)
 
 app.listen(3000, () => {
   console.log("Server running in port 3000")
